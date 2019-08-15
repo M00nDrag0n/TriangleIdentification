33fa5930ddfe57e5403de685054061b54fe73bc0
Anise Lester and Rachael Bouissey <rachael@>
2019-08-15 14:22:46 -0700

    Add a form to enter sides of a triangle, jquery not done.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+ function add(number1, number2, number3){
+   return number1 + number2 + number3;
+};
+
+
+
+
+
+
+$(document).ready(function(){
+
+  $(".formOne").submit(function(event){
+
+    var side1Input = $("input#side1").val();
+    var side2Input = $("input#side2").val();
+    var side3Input = $("input#side3").val();
+
+
+
+
+
+
+event.preventDefault();
+  });
+});
